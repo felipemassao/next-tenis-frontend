9bd1e0bcd5344d08d76bfc366b9d7f6a71378e72
William Quirino Ferreira <wqferreira@gmail.com>
2021-06-02 10:54:11 -0300

    ajustes front

---
+++ src/api/index.js
@@ -0,0 +1,1 @@
+export { default as getProduto } from './produto';
---
+++ src/hooks/useGetProduto.jsx
@@ -0,0 +1,27 @@
+import {useEffect, useState} from 'react';
+//import _ from 'lodash';
+import {getProduto} from "../api";
+
+const useGetProduto = (Id) => {
+    const [loading, setLoading] = useState(true);
+    const [Produto, setProduto] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (Id) {
+            getProduto(Id).then((result) => {
+                setError(null);
+                setProduto(result);
+                setLoading(false);
+            }).catch((error) => {
+                setLoading(false);
+                setProduto(null);
+                setError(error.message);
+            });
+        }
+    }, [Id]);
+
+    return {loading, error, Produto};
+};
+
+export default useGetProduto;
